66e06b0027af2b676970666ca01a8ed926fedcce
yulansp <1301481108@qq.com>
2020-05-20 12:14:08 +0800

    Update setup.py
    update steup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiomyorm",
-    version="0.1.2",
+    version="0.2.0",
     author="yulansp",
     author_email="1301481108@qq.com",
 
