7c1d69308a4a16218df7b149966f8589e67e62c6
yulansp <yulansp@qq.com>
2021-01-15 17:58:03 +0800

    Fix a small bug that causes incorrect results when the value of field is 0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiomyorm",
-    version="0.2.0",
+    version="0.2.1",
     author="yulansp",
     author_email="1301481108@qq.com",
 
